ed1b84ce715c676cd1e0ba31610df8128809888d
Dmitry <smallsoda@users.noreply.github.com>
2018-08-16 12:12:37 +0400

    PyCharm test

--- fs-i6.py
+++ fs-i6.py
@@ -10,62 +10,66 @@ ACTIVE_CH = 6
 
 
 def main_loop():
-	port = serial.Serial('/dev/ttyS0', 115200, timeout=0.01)
-	ch = []
-	for i in range(NUMBER_OF_CH):
-		ch.append(0)
+    port = serial.Serial('/dev/ttyS0', 115200, timeout=0.01)
+    ch = []
+    for i in range(NUMBER_OF_CH):
+        ch.append(0)
 
-	try:
-		while True:
-			data = port.read(INPUT_BUFFER_SIZE)
-			msg_head = data[0] + (data[1] << 8)
-			if len(data) == INPUT_DATA_SIZE and msg_head == HEAD_WORD:
-				os.system('clear')
+    try:
+        while True:
+            data = port.read(INPUT_BUFFER_SIZE)
+            msg_head = data[0] + (data[1] << 8)
+            if len(data) == INPUT_DATA_SIZE and msg_head == HEAD_WORD:
+                os.system('clear')
 
-				for i in range(NUMBER_OF_CH):
-					ch[i] = data[i*2+2] + (data[i*2+3] << 8)
-				if check_sum(data):
-					for i in range(ACTIVE_CH):
-						print('CH{}: {} {}'.format(i+1, stat_line(ch[i]), ch[i]))
-	except KeyboardInterrupt:
-		port.close()
+                for i in range(NUMBER_OF_CH):
+                    ch[i] = data[i*2+2] + (data[i*2+3] << 8)
+                if check_sum(data):
+                    for i in range(ACTIVE_CH):
+                        print('CH{}: {} {}'.format(i+1, stat_line(ch[i]), ch[i]))
+    except KeyboardInterrupt:
+        port.close()
 
 
 def stat_line(value, min_val=1000, max_val=2000, length=50):
-	resolution = (max_val - min_val) / length #20
-	line = ''
+    resolution = (max_val - min_val) / length #20
+    line = ''
 
-	# Left
-	for i in range(int(length/2)):
-		if value <= (min_val + i * resolution):
-			line += '|'
-		else:
-			line += ' '
+    # Left
+    for i in range(int(length/2)):
+        if value <= (min_val + i * resolution):
+            line += '|'
+        else:
+            line += ' '
 
-	# Center
-	line += '|'
+    # Center
+    line += '|'
 
-	# Right
-	for i in range(int(length/2)-1, -1, -1):
-		if value >= (max_val - i * resolution):
-			line += '|'
-		else:
-			line += ' '
+    # Right
+    for i in range(int(length/2)-1, -1, -1):
+        if value >= (max_val - i * resolution):
+            line += '|'
+        else:
+            line += ' '
 
-	return line
+    return line
 
 
 def check_sum(data):
-	sum = 0
-	for i in range(INPUT_DATA_SIZE - 2):
-		sum += data[i]
-	sum += data[INPUT_DATA_SIZE-2] + (data[INPUT_DATA_SIZE-1] << 8)
+    sum = 0
+    for i in range(INPUT_DATA_SIZE - 2):
+        sum += data[i]
+    sum += data[INPUT_DATA_SIZE-2] + (data[INPUT_DATA_SIZE-1] << 8)
 
-	if sum == 0xFFFF:
-		return True
-	else:
-		return False
+    if sum == 0xFFFF:
+        return True
+    else:
+        return False
+
+
+def test_func():
+    pass
 
 
 if __name__ == '__main__':
-	main_loop()
+    main_loop()
